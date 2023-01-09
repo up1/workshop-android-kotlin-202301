a2a6ddc05422305bf596fec028e4c08608e40979
up1 <somkiat.p@gmail.com>
2023-01-09 14:18:03 +0700

    Binding element with code

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,15 +2,29 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.WindowManager
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_SECURE,
-            WindowManager.LayoutParams.FLAG_SECURE)
+        myButton = findViewById(R.id.button)
+        myButton.setOnClickListener {
+            println("Clicked ...")
+            doSth()
+        }
     }
+
+    private fun doSth() {
+        TODO("Not yet implemented")
+    }
+
+
 }
+
