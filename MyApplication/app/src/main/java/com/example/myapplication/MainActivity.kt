0ea88b153b6a0693dc0d47f47347fb5d8e242bb6
up1 <somkiat.p@gmail.com>
2023-01-09 15:19:40 +0700

    Working with shared preferences

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -23,12 +24,23 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun doSth() {
+        createDataInSharedPreferences()
+
+
         val intent = Intent(this, SecondActivity::class.java)
         intent.putExtra("xxx", 123)
         startActivity(intent)
 
         // Delete current activity
         finish()
+    }
+
+    private fun createDataInSharedPreferences() {
+        val prefs = getSharedPreferences("prefs",
+            Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putString("message", "Hello sth")
+        editor.apply()
     }
 
 
--- MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.kt
+++ MyApplication/app/src/main/java/com/example/myapplication/SecondActivity.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,17 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
+
+        val result = intent.getIntExtra("xxx", -1)
+        println("My result => $result")
+
+        readData()
+    }
+
+    private fun readData() {
+        val prefs = getSharedPreferences("prefs",
+            Context.MODE_PRIVATE)
+        val data = prefs.getString("message", "not found")
+        println("Result from shared preferences => $data")
     }
 }
